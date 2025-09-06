eabaaf8e79196a0a0732d4dd7fd439981e5c6d07
agevavava <holhokgorgok@gmail.com>
2025-09-06 18:10:48 +0700

    wow

--- src/NasiGoreng.php
+++ src/NasiGoreng.php
@@ -6,6 +6,11 @@ namespace NasgorApp;
 
 class NasiGoreng
 {
+    public string $bahan1 = "nasi";
+    public string $bahan2 = "minyak";
+    public string $bahan3 = "bumbu-bumbu";
+    public string $alat = "kompor";
+
     public function resep(): void
     {
         echo 'Nasi, minyak, bumbu, api';
